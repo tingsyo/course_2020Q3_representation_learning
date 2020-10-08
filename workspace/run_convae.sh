c49ce55ce5633a778086402b1a9c2224b5621dfb
Ting-Shuo Yo <tingshuo.yo@googlemail.com>
2020-10-08 12:05:53 +0800

    - Modified experiement script for performance   - Fixed GPU driver   - Most computation is still on CPU

--- workspace/run_convae.sh
+++ workspace/run_convae.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python ../utils/convae_noaagridsatb1.py -i ../data/noaa/ -o noaa_convae -l noaa_convae.log -b 16 -r 121 -e 30 &
+python ../utils/convae_noaagridsatb1.py -i ../data/noaa/ -o noaa_convae -l noaa_convae.log -b 8 -r 121 -e 10 &
 
